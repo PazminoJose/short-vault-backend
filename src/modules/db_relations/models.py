3414415ab53511c60269f4f263ed3c14be228ffb
José Pazmiño <josepf.dev@gmail.com>
2026-01-19 00:25:17 -0500

    feat: refactor UserTag and ShortTag models to keep relationships at model level

--- src/modules/db_relations/models.py
+++ src/modules/db_relations/models.py
@@ -1,11 +1,14 @@
+from pydantic import Field
 from core.models import BaseTableModel
-from modules.db_relations.schemas import ShortTagBase, UserTagBase
-
 
 # NOTE: Model for UserTag relation
-class UserTag(BaseTableModel, UserTagBase, table=True):
+class UserTag(BaseTableModel, table=True):
+    user_id: int = Field(foreign_key="user.id")
+    tag_id: int = Field(foreign_key="tag.id")
     pass
 
 # NOTE: Model for ShortTag relation
-class ShortTag(BaseTableModel, ShortTagBase, table=True):
+class ShortTag(BaseTableModel, table=True):
+    short_id: int = Field(foreign_key="short.id")
+    tag_id: int = Field(foreign_key="tag.id")
     pass
