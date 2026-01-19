15131dedb41824642b04ff10a37a794cfe37da86
José Pazmiño <josepf.dev@gmail.com>
2026-01-19 00:18:28 -0500

    feat: add relationship between Tag and Short models

--- src/modules/tags/models.py
+++ src/modules/tags/models.py
@@ -1,6 +1,10 @@
+from sqlmodel import Relationship
 from core.models import BaseTableModel
+from modules.db_relations.models import ShortTag
 from modules.tags.schemas import TagBase
+from modules.shorts.models import Short
 
 
 class Tag(BaseTableModel, TagBase, table=True):
+    shorts: list["Short"] = Relationship(back_populates="tags", link_model=ShortTag) 
     pass
